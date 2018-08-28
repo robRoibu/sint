e7ede3e84adddc8feae2c228c0b2f98d2b20c0af
klmanion <klmanion@gmail.com>
2018-08-28 17:49:29 -0400

    added src folder and AUTHORS

---
+++ AUTHORS.md
@@ -0,0 +1,5 @@
+# sint Development Team
+
+[Rob N. Roibu](//github.com/robRoibu)
+
+[Kurt L. Manion](//github.com/klmanion)
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/*-
+ *main.cpp
+ * created by: Rob N. Roibu and Kurt L. Manion
+ * on: Tues., 28 Aug. 2018
+ * simple integration calculator
+ */
+
+/* DESIDERATUM
+ * TODO
+ */
+
+#include <cstdlib>
+
+int
+main(
+	int argc,
+	char *const argv[])
+{
+	return EXIT_SUCCESS;
+}
+
+/* vim: set ts=8 sw=8 noexpandtab tw=79: */
